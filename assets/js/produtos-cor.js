1f7a0a1edb7b319de99ff7528dfc3ab3ebe7b8cd
yara baccule <yarabaccule@yahoo.com.br>
2022-12-01 21:41:00 -0300

    Terceira-tentativa

--- assets/js/produtos-cor.js
+++ assets/js/produtos-cor.js
@@ -1,22 +1,29 @@
-
-const localCor = document.querySelectorAll("[local]");
-const tipoCor = document.querySelectorAll("[tipo]");
+const localCor = document.querySelectorAll("[local]")
+const tipoCor = document.querySelectorAll("[tipo]")
 
 localCor.forEach( (elemento) =>{
     elemento.addEventListener("click", (evento)=> {
         cor = evento.target.getAttribute('local')
         verCores(cor)
     })
-});
+})
 
 function verCores(padrao){
     for (var i = 0; i < tipoCor.length; i++){
         atual = tipoCor[i].getAttribute('tipo')
         if (padrao === atual){
-            tipoCor[i].style.visibility = "visible"
+            tipoCor[i].style.display = "block"
         } else if (padrao != atual){
             tipoCor[i].style.display = "none"
         }
-
+        botaoVoltar(tipoCor[i])
     }
-};
+
+}
+
+function botaoVoltar(cores){
+    botao = document.getElementById('menu')
+    botao.addEventListener("click", ()=>{
+        cores.style.display = "block"
+    })
+}
